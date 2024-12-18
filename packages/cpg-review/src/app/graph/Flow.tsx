c5dcfa4ab0ad0f3a8f95eb78bb1577621ca45831
taylorkle <114777370+taylorkle@users.noreply.github.com>
2024-12-18 10:27:24 -0500

    CPG Review - Support questionnaire node

--- packages/cpg-review/src/app/graph/Flow.tsx
+++ packages/cpg-review/src/app/graph/Flow.tsx
@@ -209,7 +209,7 @@ class Flow implements FlowShape {
             resolver,
             edgeSource
           )
-        } else if (is.ActivityDefinition(definition) && definition.id) {
+        } else if ((is.ActivityDefinition(definition) || is.Questionnaire(definition)) && definition.id) {
           const id = definition.id
           let activityNode = this.nodes?.find((n) => n.id === id)
           if (activityNode == null) {
